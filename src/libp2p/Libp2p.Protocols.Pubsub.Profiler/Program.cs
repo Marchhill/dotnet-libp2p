75342dc33644011c72ca208f9ce901a88a87a6f2
Marc Harvey-Hill <Marchhill@users.noreply.github.com>
2024-10-11 10:23:50 +0100

    pubsub experiment

--- src/libp2p/Libp2p.Protocols.Pubsub.Profiler/Program.cs
+++ src/libp2p/Libp2p.Protocols.Pubsub.Profiler/Program.cs
@@ -9,9 +9,10 @@ using Nethermind.Libp2p.Core.TestsBase;
 using Nethermind.Libp2p.Core.TestsBase.E2e;
 using Nethermind.Libp2p.Protocols;
 using Nethermind.Libp2p.Protocols.Pubsub;
+using Nethermind.Libp2p.Stack;
 using System.Text;
 
-int totalCount = 5;
+int totalCount = 20;
 TestContextLoggerFactory fac = new();
 // There is common communication point
 ChannelBus commonBus = new(fac);
@@ -24,11 +25,21 @@ for (int i = 0; i < totalCount; i++)
 {
     // But we create a seprate setup for every peer
     ServiceProvider sp = new ServiceCollection()
-           .AddSingleton(sp => new TestBuilder(commonBus, sp).AddAppLayerProtocol<GossipsubProtocol>())
-           .AddSingleton<ILoggerFactory>(sp => fac)
-           .AddSingleton<PubsubRouter>()
-           .AddSingleton<PeerStore>()
-           .AddSingleton(sp => sp.GetService<IPeerFactoryBuilder>()!.Build())
+            .AddLibp2p(sp => sp)
+            .AddSingleton(sp => new TestBuilder(commonBus, sp).AddAppLayerProtocol<GossipsubProtocol>())
+            // .AddSingleton<ILoggerFactory>(sp => fac)
+            .AddSingleton<PubsubRouter>()
+            .AddSingleton<PeerStore>()
+            .AddSingleton(sp => sp.GetService<IPeerFactoryBuilder>()!.Build())
+            // .AddSingleton<ILoggerFactory>(new NethermindLoggerFactory(logManager, overrideAllWith: Microsoft.Extensions.Logging.LogLevel.Information))
+            .AddLogging(builder =>
+                builder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace)
+                .AddSimpleConsole(l =>
+                {
+                    l.SingleLine = true;
+                    l.TimestampFormat = "[HH:mm:ss.FFF]";
+                })
+            )
            .BuildServiceProvider();
 
     IPeerFactory peerFactory = sp.GetService<IPeerFactory>()!;
@@ -46,44 +57,65 @@ for (int i = 0; i < peers.Length; i++)
     peerStores[i].Discover([peers[(i + 1) % totalCount].Address]);
 }
 
-await Task.Delay(10000);
+// await Task.Delay(10000);
 
-Console.WriteLine("Routers");
+// Console.WriteLine("Routers");
 
-for (int i = 0; i < routers.Length; i++)
-{
-    Console.WriteLine(routers[i].ToString());
-}
+// for (int i = 0; i < routers.Length; i++)
+// {
+//     Console.WriteLine(routers[i].ToString());
+// }
 
-Console.WriteLine("Stores");
+// Console.WriteLine("Stores");
 
-for (int i = 0; i < peerStores.Length; i++)
-{
-    Console.WriteLine(peerStores[i].ToString());
-}
+// for (int i = 0; i < peerStores.Length; i++)
+// {
+//     Console.WriteLine(peerStores[i].ToString());
+// }
 
-for (int i = 0; i < routers.Length; i++)
-{
-    routers[i].GetTopic("test");
-}
+// for (int i = 0; i < routers.Length; i++)
+// {
+//     routers[i].GetTopic("test");
+// }
 
 await Task.Delay(5000);
 
 var testTopic = routers[1].GetTopic("test");
-var testTopicEnd = routers[totalCount - 1].GetTopic("test");
-testTopicEnd.OnMessage += (s) => Console.WriteLine(Encoding.UTF8.GetString(s));
+// var testTopicEnd = routers[totalCount - 1].GetTopic("test");
+// testTopicEnd.OnMessage += (s) => Console.WriteLine(Encoding.UTF8.GetString(s));
 
-testTopic.Publish(Encoding.UTF8.GetBytes("test"));
-
-for (int i = 0; i < 20; i++)
-{
-    Console.WriteLine(i * 100);
-    await Task.Delay(100);
-}
-
-Console.WriteLine("Routers");
-
+int recvCount = 0;
 for (int i = 0; i < routers.Length; i++)
 {
-    Console.WriteLine(routers[i].ToString());
+    int index = i;
+    Console.WriteLine("Subscribing " + index);
+    routers[index].GetTopic("test").OnMessage += (s) => {
+        Console.WriteLine(index + " recv: " + Encoding.UTF8.GetString(s));
+        Interlocked.Increment(ref recvCount);
+    };
 }
+
+await Task.Delay(10000);
+
+for (int iter = 0;; iter++)
+{
+    Console.WriteLine("Publishing " + iter);
+    testTopic.Publish(Encoding.UTF8.GetBytes("test" + iter));
+    await Task.Delay(5000);
+    Console.WriteLine("count: " + recvCount);
+    Interlocked.Exchange(ref recvCount, 0);
+    Console.WriteLine("---");
+}
+
+// for (int i = 0; i < 20; i++)
+// {
+//     Console.WriteLine(i * 100);
+//     await Task.Delay(100);
+// }
+
+// Console.WriteLine("Routers");
+
+// for (int i = 0; i < routers.Length; i++)
+// {
+//     Console.WriteLine(routers[i].ToString());
+// }
